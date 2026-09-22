f78325cd7ee888eb009fc530445ed39c516b8307
crucie <amaymishra100@gmail.com>
2026-09-22 15:38:00 +0530

    feat(auth): wire oauth routes for google and github

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -6,7 +6,16 @@ import {
   me,
   setUsername,
   checkUsername,
+  updateProfile,
 } from "../controllers/auth.controller.js";
+import {
+  oauthStatus,
+  googleAuthUrl,
+  googleCallback,
+  githubAuthUrl,
+  githubCallback,
+  unlinkGithub,
+} from "../controllers/oauth.controller.js";
 import { authenticate } from "../middlewares/auth.middleware.js";
 
 const router = Router();
@@ -15,8 +24,31 @@ router.post("/signup", signup);
 router.post("/login", login);
 router.post("/google", googleAuth);
 
+router.get("/oauth/status", oauthStatus);
+
+router.get("/google/url", googleAuthUrl);
+router.get("/google/callback", googleCallback);
+router.get("/google/connect-url", authenticate, (req, res, next) => {
+  req.oauthMode = "link";
+  return googleAuthUrl(req, res, next);
+});
+
+router.get("/github/url", githubAuthUrl);
+router.get("/github/callback", githubCallback);
+router.get("/github/connect-url", authenticate, (req, res, next) => {
+  req.oauthMode = "link";
+  return githubAuthUrl(req, res, next);
+});
+router.delete("/github", authenticate, unlinkGithub);
+
+router.post("/github-app/identity", authenticate, async (req, res, next) => {
+  const { linkGithubAppIdentity } = await import("../controllers/github.controller.js");
+  return linkGithubAppIdentity(req, res, next);
+});
+
 router.get("/me", authenticate, me);
 router.patch("/username", authenticate, setUsername);
+router.patch("/profile", authenticate, updateProfile);
 router.get("/username-available", authenticate, checkUsername);
 
 export default router;
