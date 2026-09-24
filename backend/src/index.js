6c69ffbb37252f2d9c308cedf58902061fdeca1e
crucie <amaymishra100@gmail.com>
2026-09-24 15:48:00 +0530

    feat(api): bootstrap domain and github sync on startup

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,10 @@ import "dotenv/config";
 import app from "./app.js";
 import prisma from "./config/prisma.js";
 import { bootstrapBoardColumns } from "./db/bootstrapColumns.js";
+import { bootstrapSpecDomain } from "./db/bootstrapSpecDomain.js";
+import { connectRedis } from "./config/redis.js";
+import { startTeamRedisBridge } from "./utils/teamBus.js";
+import { startGithubSyncCron } from "./services/githubSync.service.js";
 
 const PORT = process.env.PORT || 3000;
 
@@ -21,7 +25,11 @@ if (process.env.NODE_ENV === "production") {
 prisma
   .$connect()
   .then(() => bootstrapBoardColumns(prisma))
+  .then(() => bootstrapSpecDomain(prisma))
+  .then(() => connectRedis())
   .then(() => {
+    startTeamRedisBridge();
+    startGithubSyncCron();
     console.log("[fs-knbn] Database connected.");
 
     app.listen(PORT, "0.0.0.0", () => {
